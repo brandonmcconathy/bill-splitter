c012cac28d92e331dd7ccd0c43f16f478db26212
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-26 20:50:37 -0700

    Added components

---
+++ src/app/components/taxcalc.tsx
@@ -0,0 +1,5 @@
+export default function TaxCalc() {
+  return(
+    <h1>Tax Calculator</h1>
+  )
+}
---
+++ src/app/components/tipcalc.tsx
@@ -0,0 +1,5 @@
+export default function TipCalc() {
+  return(
+    <h1>Tip Calculator</h1>
+  )
+}
