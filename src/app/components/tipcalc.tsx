927144bf13a6842c2b22167c18fb9c5e64bf24bd
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-28 18:18:04 -0700

    Added content to tip calculator

--- src/app/components/tipcalc.tsx
+++ src/app/components/tipcalc.tsx
@@ -1,7 +1,19 @@
 export default function TipCalc() {
+
+  let tip = 0
+
   return(
     <section>
       <h1>Tip Calculator</h1>
+      <div>
+        <label>Subtotal Before Tax</label>
+        <input />
+      </div>
+      <div>
+        <label>Tip Amount</label>
+        <input />
+      </div>
+      <h1>Tip is {tip}%</h1>
     </section>
   )
 }
