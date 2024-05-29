bfc9a5592ec37cb50daaf4c11a7d3b7ba521af2a
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-29 16:52:53 -0700

    Built out inputs in main component

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,21 +1,31 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Main() {
 
-  let share = 0
+  const [share, setShare] = useState(0)
+  const [input, setInput] = useState({cost: 0, tax: 0, tip: 0})
+
+  const handleChange = (event:any) => {
+    const { name, value } = event.target
+    setInput((prevInput) => ({ ...prevInput, [name]: value}))
+  }
 
   return(
     <section>
       <h1>Bill Splitter</h1>
       <div>
         <label>Your Item Cost</label>
-        <input />
+        <input type="number" id='cost' name='cost' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <div>
         <label>Tax (%)</label>
-        <input />
+        <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <div>
         <label>Tip (%)</label>
-        <input />
+        <input type="number" id='tip' name='tip' value={input.tip} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <h2>You owe: {share}</h2>
     </section>
