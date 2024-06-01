bd2de8e875002b184eb80cc0bb0f82aa3c5e7655
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-31 17:06:20 -0700

    Added tax calculator logic

--- src/app/components/taxcalc.tsx
+++ src/app/components/taxcalc.tsx
@@ -1,19 +1,28 @@
+'use client'
+
+import { useState } from "react"
+
 export default function TaxCalc() {
 
-  let tax = 0
+  const [input, setInput] = useState({subtotal: 0, tax: 0})
+
+  const handleChange = (event:any) => {
+    const { name, value } = event.target
+    setInput((prevInput) => ({ ...prevInput, [name]: value}))
+  }
 
   return(
     <section>
       <h1>Tax Calculator</h1>
       <div>
         <label>Subtotal ($)</label>
-        <input type="number" id='subtotal' name='subtotal' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
+        <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <div>
         <label>Tax Amount ($)</label>
-        <input type="number" id='tax' name='tax' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
+        <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
-      <h1>Tax is {tax}%</h1>
+      <h1>Tax is {input.tax / input.subtotal}%</h1>
     </section>
   )
 }
