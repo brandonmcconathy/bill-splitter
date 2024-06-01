e8642d36273d53945d0e143a754589d0e45329ae
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-05-31 17:04:27 -0700

    Styled input on tax calculator

--- src/app/components/taxcalc.tsx
+++ src/app/components/taxcalc.tsx
@@ -6,12 +6,12 @@ export default function TaxCalc() {
     <section>
       <h1>Tax Calculator</h1>
       <div>
-        <label>Subtotal</label>
-        <input />
+        <label>Subtotal ($)</label>
+        <input type="number" id='subtotal' name='subtotal' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <div>
-        <label>Tax Amount</label>
-        <input />
+        <label>Tax Amount ($)</label>
+        <input type="number" id='tax' name='tax' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <h1>Tax is {tax}%</h1>
     </section>
