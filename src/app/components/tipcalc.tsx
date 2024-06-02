16b679dd1d969172898fd0b91787c8fe12fc78f3
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-06-01 18:11:20 -0700

    Styled input boxes for tip calc

--- src/app/components/tipcalc.tsx
+++ src/app/components/tipcalc.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { useState } from "react"
+
 export default function TipCalc() {
 
   let tip = 0
@@ -7,11 +11,11 @@ export default function TipCalc() {
       <h1>Tip Calculator</h1>
       <div>
         <label>Subtotal Before Tax</label>
-        <input />
+        <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <div>
         <label>Tip Amount</label>
-        <input />
+        <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <h1>Tip is {tip}%</h1>
     </section>
