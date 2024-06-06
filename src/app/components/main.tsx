4b6d1b964da84754a120f45f503d22406eb9d658
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-06-06 12:49:31 -0700

    Updated labels in main component

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -15,15 +15,15 @@ export default function Main() {
     <section className="bg-amber-200 flex flex-col justify-center items-center my-12 py-4 mx-auto gap-4 font-semibold rounded-xl box-pop w-1/2">
       <h1 className="text-2xl font-bold">Bill Splitter</h1>
       <div className="flex gap-4 items-center justify-center">
-        <label>Your Item Cost ($):</label>
+        <label htmlFor="cost">Your Item Cost ($):</label>
         <input type="number" id='cost' name='cost' value={input.cost} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300 w-1/4" />
       </div>
       <div className="flex gap-4 items-center justify-center">
-        <label>Tax (%):</label>
+        <label htmlFor="tax">Tax (%):</label>
         <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300 w-1/4" />
       </div>
       <div className="flex gap-4 items-center justify-center">
-        <label>Tip (%):</label>
+        <label htmlFor="tip">Tip (%):</label>
         <input type="number" id='tip' name='tip' value={input.tip} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300 w-1/4" />
       </div>
       <h2 className="text-lg">You owe: ${Math.round((Number(input.cost) * ((input.tax / 100) + (input.tip / 100) + 1)) * 100) / 100}</h2>
