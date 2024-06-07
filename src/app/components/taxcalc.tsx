68504b7b81f3179dcba0364cf2821dc09685ece1
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-06-07 09:15:10 -0700

    Styled tax calc

--- src/app/components/taxcalc.tsx
+++ src/app/components/taxcalc.tsx
@@ -13,13 +13,13 @@ export default function TaxCalc() {
 
   return(
     <section className="bg-amber-200 flex flex-col justify-center items-center my-12 py-4 mx-auto gap-4 font-semibold rounded-xl box-pop w-1/2">
-      <h1>Tax Calculator</h1>
-      <div>
-        <label>Subtotal ($)</label>
+      <h1 className="text-2xl font-bold">Tax Calculator</h1>
+      <div className="flex gap-4 items-center justify-center">
+        <label>Subtotal ($):</label>
         <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
-      <div>
-        <label>Tax Amount ($)</label>
+      <div className="flex gap-4 items-center justify-center">
+        <label>Tax Amount ($):</label>
         <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
       </div>
       <h1>Tax is {Math.round((input.tax / input.subtotal) * 10000) / 100}%</h1>
