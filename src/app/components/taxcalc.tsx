cabb56a1480c42a14cac16ab260dbda2e7bc5f09
Brandon McConathy <brandonmcconathy4321@gmail.com>
2024-06-09 22:27:10 -0700

    Styled tax calc

--- src/app/components/taxcalc.tsx
+++ src/app/components/taxcalc.tsx
@@ -12,17 +12,17 @@ export default function TaxCalc() {
   }
 
   return(
-    <section className="bg-amber-200 flex flex-col justify-center items-center my-12 py-4 mx-auto gap-4 font-semibold rounded-xl box-pop w-1/2">
+    <section className="bg-amber-200 flex flex-col justify-center items-center my-20 py-4 mx-auto gap-4 font-semibold rounded-xl box-pop w-1/2">
       <h1 className="text-2xl font-bold">Tax Calculator</h1>
       <div className="flex gap-4 items-center justify-center">
         <label htmlFor="subtotal">Subtotal ($):</label>
-        <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
+        <input type="number" id='subtotal' name='subtotal' value={input.subtotal} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300 w-1/4" />
       </div>
       <div className="flex gap-4 items-center justify-center">
         <label htmlFor="tax">Tax Amount ($):</label>
-        <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300" />
+        <input type="number" id='tax' name='tax' value={input.tax} onChange={handleChange} required className="text-black outline-none rounded-xl px-4 py-2 shadow-xl focus:ring focus:ring-gray-300 transition duration-300 w-1/4" />
       </div>
-      <h1>Tax is {Math.round((input.tax / input.subtotal) * 10000) / 100}%</h1>
+      <h1 className="text-lg">Tax is {Math.round((input.tax / input.subtotal) * 10000) / 100}%</h1>
     </section>
   )
 }
